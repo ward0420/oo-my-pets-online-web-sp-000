68c9d2034f5dbbd8eb868756b097d73c7cdf15ba
Don Ward <10is0420@gmail.com>
2019-08-17 17:34:04 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -75,7 +75,7 @@ class Owner
   def list_pets
     buy_cat(name)
     buy_dog(name)
-    puts "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+    return "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
   end
   
   
